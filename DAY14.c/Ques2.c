58553d1133436dbee5f718d9f24ae0f94186c7c5
VarnikaRana <varnika025rana@gmail.com>
2026-02-14 08:21:38 +0530

    codes

---
+++ DAY14.c/Ques2.c
@@ -0,0 +1,67 @@
+//You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+//You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+    int n = matrixSize; // n x n matrix
+
+    // Step 1: Transpose the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (int i = 0; i < n; i++) {
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int temp = matrix[i][left];
+            matrix[i][left] = matrix[i][right];
+            matrix[i][right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    // Allocate matrix (int **)
+    int **matrix = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (int *)malloc(n * sizeof(int));
+    }
+
+    // Input matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int colSize = n;
+    rotate(matrix, n, &colSize);
+
+    // Print rotated matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Free memory
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
+}
